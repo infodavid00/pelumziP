cf2e90ff0a2b75bcd2e086ddd4ffd285ae75f129
infodavid00 <da40au40@gmail.com>
2024-07-31 11:19:40 +0100

    ...

--- chronos/src/local/start/start.js
+++ chronos/src/local/start/start.js
@@ -1,16 +1,15 @@
-
 "use strict";
 
 import ANSI from "../../var/ansi.js";
 import cli from "../cli/cli.js";
-import { createServer } from "node:http"; 
+import { createServer } from "node:http";
 import gateway from "../../lib/gateway.js";
 import jobs_update_cache from "../../lib/jobs/jobs.updateCache.js";
 import server_options from "../../global/server.options.js";
 
 /**
  * The function that starts the server and any neccessary background job.
- * 
+ *
  * @author David.A super-user-d0
  */
 export default function start() {
@@ -23,18 +22,16 @@ export default function start() {
     const url = `http://${address}:${port}`;
     console.log();
     jobs_update_cache();
-    if (server_options().getInnerOptions().enableCLI) cli(url)
+    if (server_options().getInnerOptions().enableCLI) cli(url);
     else {
-      const reason = process.argv.includes("::disable-cli") ? 
-        "the presence of the \`::disable-cli\` command line argument." : 
-        "the lack of support for ANSI commands on your terminal.";
-      console.log(`${ansi.dim} The Chronos CLI was disabled due to ${reason}${ansi.reset}`);
+      const reason = process.argv.includes("::disable-cli")
+        ? "the presence of the `::disable-cli` command line argument."
+        : "the lack of support for ANSI commands on your terminal.";
+      console.log(
+        `${ansi.dim} The Chronos CLI was disabled due to ${reason}${ansi.reset}`
+      );
     }
   };
 
-  server.listen(
-     port, 
-     address, 
-     onServerStart
-  );
+  server.listen(port, onServerStart);
 }
